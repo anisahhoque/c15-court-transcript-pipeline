d8846d0e05d7cc6bf8fd24d32076d19d35611eca
anisahhoque <hoqueshabrina@gmail.com>
2025-02-14 16:51:25 +0000

    somewhat viable solution

--- prompt-engineering/prompt_engineering.py
+++ prompt-engineering/prompt_engineering.py
@@ -24,10 +24,7 @@ class Judgment(BaseModel):
     """Reference/unique id for a judgment"""
     neutral_citation: str
 
-class Argument(BaseModel):
-    summary: str
-    judgments_referenced: list[Judgment]
-    legislations_referenced: list[Legislation]
+
 
 class Counsel(BaseModel):
     """Represents a counsel for a party"""
@@ -41,7 +38,13 @@ class Party(BaseModel):
     party_name: str
     party_role: str
     counsels: list[Counsel]
-    arguments: list[Argument]
+
+
+class Argument(BaseModel):
+    summary: str
+    judgments_referenced: list[Judgment]
+    legislations_referenced: list[Legislation]
+    party_role: str
 
 
 class CaseOutput(BaseModel):
@@ -50,6 +53,7 @@ class CaseOutput(BaseModel):
     description: str
     judge: list[Judge]
     parties: list[Party]
+    arguments: list[Argument]
 
 class JudgmentOutput(BaseModel):
     """Returns all information in a case summary"""
@@ -103,8 +107,8 @@ def get_case_summaries(model: str, client: OpenAI, prompt: str) -> list[dict]:
 if __name__=="__main__":
     api_client = get_client()
     GPT_MODEL = "o3-mini"
-    file_names = ['ewhc_comm_2025_240.xml','ukut_iac_2021_202.xml','ewcop_t3_2025_6.xml']
-    list_of_cases = get_list_xml_data(filenames=[file_names[1]])
+    file_names = ['ewhc_comm_2025_240.xml','ukut_iac_2021_202.xml','ewcop_t3_2025_6.xml', 'ewhc_kb_2025_287.xml']
+    list_of_cases = get_list_xml_data(filenames=[file_names[3]])
     PROMPT = f"""
     I have a list of court case transcriptions:
     {list_of_cases}
@@ -113,8 +117,7 @@ if __name__=="__main__":
 
     Give me a well rounded summary for each of the judgments provided. Your response should be in a list of dictionaries containing the following keys:
     - type_of_crime: criminal or civil 
-    - description: a short neutral summary of the trial
-    - judge: The fullname of the judge including the title e.g Mr Justice Smith
+    - description: a short neutral summary of the judgment
     - parties: A list of all parties involved in the case, with the following details for each party:
         - name: The name of the party.
         - role: The role of the party (e.g., "plaintiff", "defendant", "appellant", "respondent").
@@ -122,11 +125,15 @@ if __name__=="__main__":
             - name: The name of the counsel (e.g., "William Bennett KC").
             - title: The title of the counsel (e.g., "KC", "QC", or none).
             - chamber: The name of the counsel's chambers (e.g., "Brett Wilson LLP").
-        - arguments: A list of unique arguments, claims, or submissions made by the party. For each argument, include:
-            - A brief summary of the argument presented
-            - a list of references(neutral citations) that are mentioned in that specific argument
-            - a list of legislations that are mentioned in that specific argument
-            - legislations have a legislation name and section for example Data Protection Act 2018 Section 17
+    - arguments: a list of as many unique arguments as you can find made throughout the judgment that are made by the roles of which parties that you have found. Each argument contains:
+                - A brief summary of the argument presented
+                - A list of judgments that are mentioned in that specific argument. Use only their neutral citation number e.g., [2025] EWHC 287 (KB).
+                - a list of legislations that are mentioned in that specific argument
+                - legislations have a legislation name and section for example Data Protection Act 2018 Section 17
+                - can you assign the role
+    - judge: The fullname of the judge including the title e.g Mr Justice Smith
+
+
 
 
 
