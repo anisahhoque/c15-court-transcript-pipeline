2b095a1fdfe5bce39875a6231c45a4ef8dee5e3e
Bill <bvelizmera@gmail.com>
2025-02-17 15:49:56 +0000

    added changes to schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -5,7 +5,9 @@ DROP TABLE IF EXISTS counsel;
 DROP TABLE IF EXISTS judgment;
 DROP TABLE IF EXISTS court;
 DROP TABLE IF EXISTS judgment_type;
+DROP TABLE IF EXISTS chamber;
 DROP TABLE IF EXISTS role;
+
 
 CREATE TABLE role (
     role_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -24,10 +26,16 @@ CREATE TABLE judgment_type (
     judgment_type VARCHAR(30)
 );
 
+CREATE TABLE chamber (
+    chamber_id SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    chamber_name VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE counsel (
     counsel_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     counsel_name VARCHAR(100) NOT NULL,
-    chamber_name VARCHAR(30) NOT NULL
+    chamber_id SMALLINT,
+    CONSTRAINT fk_chamber FOREIGN KEY (chamber_id) REFERENCES chamber (chamber_id)
 );
 
 CREATE TABLE judgment (
