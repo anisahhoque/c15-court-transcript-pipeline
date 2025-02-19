fe2b4358c5862bbaf730a5050d9e165e66bb16cc
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2025-02-19 11:22:35 +0000

    Adds historical pipeline ECR repo url

--- cloud-resources/outputs.tf
+++ cloud-resources/outputs.tf
@@ -5,3 +5,7 @@ output "pipeline_ecr_url" {
 output "server_ecr_url" {
   value = aws_ecr_repository.server.repository_url
 }
+
+output "historical_pipeline_ecr_url" {
+  value = aws_ecr_repository.hsitorical_pipeline.repository_url
+}
