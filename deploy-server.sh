1c7102d8d87be892b133674bb2189f0cb41710cb
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2025-02-19 21:14:16 +0000

    Fixes environment variable naming issue

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,6 +1,6 @@
 source .env
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com
 docker build --platform linux/arm64 --provenance false -t judgment-reader-server streamlit/.
-docker tag judgment-reader-server ${SERVER_ECR_REPOSITORY_URL}:latest
-docker push ${SERVER_ECR_REPOSITORY_URL}:latest
+docker tag judgment-reader-server ${SERVER_ECR_URL}:latest
+docker push ${SERVER_ECR_URL}:latest
 aws ecs update-service --region eu-west-2 --cluster judgment-reader --service judgment-reader-server --task-definition judgment-reader-server:latest --force-new-deployment
