358ea6903a467482b8f060b0c0ec1e0a7d1543b8
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2025-02-19 21:46:43 +0000

    Renames variable in historical pipeline deployment script

--- deploy-historical-pipeline.sh
+++ deploy-historical-pipeline.sh
@@ -1,9 +1,9 @@
 source .env
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com
-if [ $# -eq 0 ] || [ -z "$1" ]
-  then
-    docker build --platform linux/arm64 --provenance false -t judgment-reader-historical-pipeline seed_data/.
-  else
-    docker build --platform linux/arm64 --provenance false -t judgment-reader-historical-pipeline -e DAYS_TO_SEED=$1 seed_data/.
-docker tag pigasus-dashbaord:latest ${HISTORICAL_PIPELINE_ECR_URL}:latest
+if [ $# -eq 0 ] || [ -z "$1" ]; then
+  docker build --platform linux/arm64 --provenance false -t judgment-reader-historical-pipeline seed_data/.
+else
+  docker build --platform linux/arm64 --provenance false -t judgment-reader-historical-pipeline -e DAYS_TO_SEED=$1 seed_data/.
+fi
+docker tag judgment-reader-historical-pipeline ${HISTORICAL_PIPELINE_ECR_URL}:latest
 docker push ${HISTORICAL_PIPELINE_ECR_URL}:latest
