2245cb5565db1eb410d0418473b78c9715ee3da8
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2025-02-19 11:25:14 +0000

    Updates order of outputs to match README

--- cloud-resources/outputs.tf
+++ cloud-resources/outputs.tf
@@ -1,11 +1,11 @@
+output "historical_pipeline_ecr_url" {
+  value = aws_ecr_repository.hsitorical_pipeline.repository_url
+}
+
 output "pipeline_ecr_url" {
   value = aws_ecr_repository.pipeline.repository_url
 }
 
 output "server_ecr_url" {
   value = aws_ecr_repository.server.repository_url
-}
-
-output "historical_pipeline_ecr_url" {
-  value = aws_ecr_repository.hsitorical_pipeline.repository_url
 }
