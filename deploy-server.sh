86723748f70a32b12f76b5b742f43b7f1d95fbee
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2025-02-19 12:15:42 +0000

    Adds deploy-server script

---
+++ deploy-server.sh
@@ -0,0 +1,5 @@
+source .env
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com
+docker build --platform linux/arm64 --provenance false -t judgment-reader-server streamlit/.
+docker tag pigasus-dashbaord:latest ${SERVER_ECR_REPOSITORY_URL}:latest
+docker push ${SERVER_ECR_REPOSITORY_URL}:latest
