c9ed79c7d1e5b5e30d634eb0b83011012940e044
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2025-02-19 12:19:59 +0000

    Forces redeployment of server following new upload

--- deploy-server.sh
+++ deploy-server.sh
@@ -3,3 +3,4 @@ aws ecr get-login-password --region eu-west-2 | docker login --username AWS --pa
 docker build --platform linux/arm64 --provenance false -t judgment-reader-server streamlit/.
 docker tag pigasus-dashbaord:latest ${SERVER_ECR_REPOSITORY_URL}:latest
 docker push ${SERVER_ECR_REPOSITORY_URL}:latest
+aws ecs update-service --region eu-west-2 --cluster judgment-reader --service judgment-reader-server --task-definition judgment-reader-server:latest --force-new-deployment
