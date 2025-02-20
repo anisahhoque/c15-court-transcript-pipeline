b3400c97c4bfbd35f4dab2b4a726a61f8bb016f6
Aren <111581520+akev2794@users.noreply.github.com>
2025-02-20 21:24:51 +0000

    Added bash script for deploying SES lambda

---
+++ deploy-ses-lambda.sh
@@ -0,0 +1,5 @@
+source .env
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com
+docker build --platform linux/arm64 --provenance false -t judgment-reader-report-lambda ses_lambda/.
+docker tag judgment-reader-report-lambda ${REPORT_LAMBDA_ECR_URL}:latest
+docker push ${REPORT_LAMBDA_ECR_URL}:latest
