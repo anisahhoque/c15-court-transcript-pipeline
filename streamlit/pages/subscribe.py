3fa26e61cb0957df02a4f4ba0a2e0daf0c97b554
Aren <111581520+akev2794@users.noreply.github.com>
2025-02-20 21:33:31 +0000

    Added subscribe page to streamlit

---
+++ streamlit/pages/subscribe.py
@@ -0,0 +1,15 @@
+"""Page to subscribe using email to daily judgment reports."""
+from os import environ as ENV
+
+import streamlit as st
+
+from streamlit.subscribe_functions import is_valid_email, create_client, create_contact
+
+if __name__ == "__main__":
+    email_input = st.text_input("Enter your email here: ")
+    if st.button("Subscribe to daily judgment emails"):
+        if is_valid_email(email_input.strip()):
+            with create_client(ENV["ACCESS_KEY"], ENV["SECRET_KEY"]) as ses_client:
+                create_contact(ses_client, ENV["CONTACT_LIST_NAME"], email_input.strip())
+        else:
+            st.error("Please ensure your email is valid.")
